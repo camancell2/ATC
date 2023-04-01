9d5b2c5da700597632b72473342ce6a73ac03997
zzVertigo <zz@vertigo.rocks>
2023-04-01 00:09:57 -0700

    Tried my best to 'document' any work done and explain what it does

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,7 @@
 const utils = require('../utils.js');
 
 const homeView = async (req, res) => {
+    // See utils.js:6
     const recentPosts = await utils.GetPosts({"postDate": 'desc'}, 10);
     const mostLiked = await utils.GetPosts({"liked.length": 'asc'}, 5);
     
